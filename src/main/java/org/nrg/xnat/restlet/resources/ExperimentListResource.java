6ee0414681d4a21dd854c8de99080d4c578b723e
James Ransford <ransfordj@radiologics.com>
2018-01-22 16:02:16 -0600

    Make FilteredExptListHandlerI public

--- src/main/java/org/nrg/xnat/restlet/resources/ExperimentListResource.java
+++ src/main/java/org/nrg/xnat/restlet/resources/ExperimentListResource.java
@@ -169,7 +169,7 @@ public class ExperimentListResource  extends QueryOrganizerResource {
     }
     
     //FilteredExptListHandlerI allows additional experiment list handlers to be added via modules
-    interface FilteredExptListHandlerI {
+    public interface FilteredExptListHandlerI {
     	boolean canHandle(SecureResource resource);
     	XFTTable build(ExperimentListResource resource,Hashtable<String,Object> params) throws Exception;
     }
