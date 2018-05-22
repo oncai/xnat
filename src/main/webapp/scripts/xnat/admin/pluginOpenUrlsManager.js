c1610521b938101946d433d4bca06e9bae9b03e9
Mark M. Florida <markflorida@wustl.edu>
2018-05-22 11:53:00 -0500

    Fixed display and element structure for 'Plugin Open URLs' admin panel.

--- src/main/webapp/scripts/xnat/admin/pluginOpenUrlsManager.js
+++ src/main/webapp/scripts/xnat/admin/pluginOpenUrlsManager.js
@@ -9,80 +9,80 @@
 
 $(function(){
 
-	console.log('pluginOpenUrlsManager.js');
+    var undef;
 
-	$.ajax({
-		type : "GET",
-		url:serverRoot + "/xapi/pluginOpenUrls/settings",
-		cache: false,
-		async: true,
-		context: this,
-		dataType: 'text'
-	})
-	.done(function(data, textStatus,jqXHR) {
+    console.log('pluginOpenUrlsManager.js');
 
-		var rObj = JSON.parse(data);
-		populateDisplay(rObj,!(typeof rObj === 'undefined' || Object.keys(rObj).length<1));
-	})
-	.fail(function(data, textStatus,jqXHR) {
-		console.log("WARNING:  Could not retrieve plugin openurls configuration status");
-		populateDisplay(undefined, false);
-	});
+    XNAT.xhr.get({
+         url: XNAT.url.restUrl("/xapi/pluginOpenUrls/settings"),
+         dataType: 'text'
+     })
+     .done(function(data, textStatus, jqXHR){
+         var rObj = JSON.parse(data);
+         populateDisplay(rObj, !(rObj === undef || Object.keys(rObj).length < 1));
+     })
+     .fail(function(data, textStatus, jqXHR){
+         console.log("WARNING:  Could not retrieve plugin openurls configuration status");
+         populateDisplay(undef, false);
+     });
 
-	var populateDisplay = function(settingsObj, anyDefined) {
+    function populateDisplay(settingsObj, anyDefined){
 
-		var urlPanel = {
-			urlPanel : {
-				id:  "openurls-panel",
-				kind:  "panel.form",
-				label: "Open URLs Defined in Plugins",
-				method: "POST",
-				contentType: "json",
-				url: "/xapi/pluginOpenUrls/settings",
-				contents: {
-					message: { tag: "div.message.bold",
-						   content: "NOTE:  Open URLs defined in plugins must be enabled by an administrator before they are available in the" +
-								" application.  A Tomcat restart is required for changes in the open URL configuration to take effect." }
-				}
-			}
-		};
-		if (anyDefined) {
-			urlPanel.urlPanel.contents["subHead"] = { kind: "panel.subhead", name:  "openurls-subhead", label: "Plugin Open URLS" };
-			for (var prop in settingsObj) {
-				if (settingsObj.hasOwnProperty(prop)) {
-					 urlPanel.urlPanel.contents[prop] = {
-							kind:  "panel.input.switchbox",
-							id:  prop,
-							label:  prop,
-							name:  prop,
-							title:  prop,
-							value:  true,
-							onText:  "Authorized",
-							offText:  "Not Authorized"
-		        		};
-				}
-			}
-		} else {
-			urlPanel.urlPanel.contents["subHead"] = {
-				 tag: "div",
-				 content: "<b>No open URLs have been defined in current plugins.</b>",
-				 element: { style: "margin-top: 20px" }
-			};
-			urlPanel.urlPanel["footer"] = false;
-		}
+        var config = {
+            urlPanel: {
+                id: "openurls-panel",
+                kind: "panel.form",
+                label: "Open URLs Defined in Plugins",
+                method: "POST",
+                contentType: "json",
+                action: "/xapi/pluginOpenUrls/settings",
+                contents: {
+                    message: {
+                        tag: "div.message.bold",
+                        content: "NOTE:  Open URLs defined in plugins must be enabled by an administrator before they are available in the" +
+                        " application.  A Tomcat restart is required for changes in the open URL configuration to take effect."
+                    }
+                }
+            }
+        };
 
-		// handle container id with or without '-panel' suffix
-		var $container = $('#plugin-open-urls-config');
-		if (!$container.length) {
+        if (anyDefined) {
+            config.urlPanel.contents["subHead"] = {
+                kind: "panel.subhead",
+                name: "openurls-subhead",
+                label: "Plugin Open URLS"
+            };
+            forOwn(settingsObj, function(prop, val){
+                config.urlPanel.contents[prop] = {
+                    kind: "panel.input.switchbox",
+                    id: prop,
+                    label: prop,
+                    name: prop,
+                    title: prop,
+                    value: true,
+                    onText: "Authorized",
+                    offText: "Not Authorized"
+                };
+            });
+        }
+        else {
+            config.urlPanel.contents["subHead"] = {
+                tag: "div",
+                content: "<b>No open URLs have been defined in current plugins.</b>",
+                element: { style: { marginTop: "20px" } }
+            };
+            config.urlPanel["footer"] = false;
+        }
+
+        // handle container id with or without '-panel' suffix
+        var $container = $('#plugin-open-urls-config');
+        if (!$container.length) {
             $container = $('#plugin-open-urls-config-panel');
-		}
+        }
 
-		XNAT.spawner.spawn(urlPanel).render($container);
+        XNAT.spawner.spawn(config).render($container);
 
-		$container.css('border', 'none');
-        $container.find('.panel-body').hide();
-
-	}
+    }
 
 });
 
