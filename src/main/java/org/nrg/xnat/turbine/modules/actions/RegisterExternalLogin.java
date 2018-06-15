cb25781b8663b67566b761696a18f2bffee80bc2
Rick Herrick <jrherrick@wustl.edu>
2018-06-14 20:55:33 -0500

    XNAT-5608 Added get/setBannerMessage methods, called from RegisterExternalLogin, added handler to DefaultTop.vm.

--- src/main/java/org/nrg/xnat/turbine/modules/actions/RegisterExternalLogin.java
+++ src/main/java/org/nrg/xnat/turbine/modules/actions/RegisterExternalLogin.java
@@ -15,6 +15,7 @@ import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.XDAT;
 import org.nrg.xdat.entities.XdatUserAuth;
+import org.nrg.xdat.security.helpers.Users;
 import org.nrg.xdat.services.XdatUserAuthService;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
 import org.nrg.xft.security.UserI;
@@ -22,6 +23,8 @@ import org.nrg.xnat.security.XnatProviderManager;
 import org.nrg.xnat.security.provider.XnatAuthenticationProvider;
 import org.nrg.xnat.security.provider.XnatDatabaseAuthenticationProvider;
 import org.nrg.xnat.security.tokens.XnatDatabaseUsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
@@ -36,60 +39,72 @@ public class RegisterExternalLogin extends XDATRegisterUser {
 
     @Override
     public void doPerform(final RunData data, final Context context) throws Exception {
-        final String operation = (String) TurbineUtils.GetPassedParameter("operation", data);
+        final String operation    = (String) TurbineUtils.GetPassedParameter("operation", data);
+        final String username     = (String) TurbineUtils.GetPassedParameter("username", data);
+        final String password     = (String) TurbineUtils.GetPassedParameter("password", data);
+        final String authUsername = (String) TurbineUtils.GetPassedParameter("authUsername", data);
+
         if (StringUtils.equals("merge", operation)) {
-            // If it's a merge with an existing account, we need to validate the username and password.
-            final String username = (String) TurbineUtils.GetPassedParameter("username", data);
-            if (StringUtils.isBlank(username)) {
-                retryAuthentication(data, context, "You must provide a valid username.");
-                return;
-            }
-            if (StringUtils.equalsIgnoreCase("guest", username)) {
-                retryAuthentication(data, context, "You can't associate an external authentication account with the guest user.");
-                return;
-            }
-
-            final String password = (String) TurbineUtils.GetPassedParameter("password", data);
-            if (StringUtils.isBlank(password)) {
-                retryAuthentication(data, context, "You must provide a password value.");
-                return;
-            }
-
             try {
-                final Authentication authentication = _provider.authenticate(new XnatDatabaseUsernamePasswordAuthenticationToken(username, password));
-
-                // If the validation failed...
-                if (!authentication.isAuthenticated()) {
-                    // Let them know and try again.
-                    retryAuthentication(data, context, "The submitted username and password didn't match an existing XNAT account.");
-                    return;
-                }
-            } catch (AuthenticationException e) {
-                log.info("User logged in with username {} through auth method {}, then tried to associate with existing XNAT account '{}' but provided invalid credentials", TurbineUtils.GetPassedParameter("authUsername", data), TurbineUtils.GetPassedParameter("authMethodId", data), username);
+                validateXnatLogin(username, password);
+                createUserAuthRecord(data, operation);
+                final UserI user = Users.getUser(username);
+                XDAT.loginUser(data, user, false);
+                TurbineUtils.setBannerMessage(data, "Your login <b>" + authUsername + "</b> is now connected to your <b>" + username + "</b> account on this XNAT server.");
+                data.setScreenTemplate("Index.vm");
+            } catch (BadCredentialsException e) {
+                log.info("User logged in with username {} through auth method {}, then tried to associate with existing XNAT account '{}' but provided invalid credentials", authUsername, TurbineUtils.GetPassedParameter("authMethodId", data), username);
                 retryAuthentication(data, context, "The submitted username and password didn't match an existing active XNAT account.");
-                return;
+            } catch (AuthenticationCredentialsNotFoundException e) {
+                retryAuthentication(data, context, "You must specify a value for both username and password.");
             }
         } else {
-            // If it's a new user, then run them through the standard registration workflow, but first
-            // add any provider-specific properties, mainly auto-enable and auto-verify.
-            final String authMethod   = data.getParameters().getString("authmethod");
-            final String providerId = data.getParameters().getString("authmethodid");
-            final XnatAuthenticationProvider provider = _manager.getProvider(authMethod, providerId);
-            if (provider != null) {
-                data.getParameters().add("authMethod", authMethod);
-                data.getParameters().add("providerId", providerId);
-                data.getParameters().add("providerAutoEnabled", Boolean.toString(provider.isAutoEnabled()));
-                data.getParameters().add("providerAutoVerified", Boolean.toString(provider.isAutoVerified()));
-            }
-            super.doPerform(data, context);
-        }
+            try {
+                // If it's a new user, then run them through the standard registration workflow, but first
+                // add any provider-specific properties, mainly auto-enable and auto-verify.
+                final String authMethod = data.getParameters().getString("authmethod");
+                final String providerId = data.getParameters().getString("authmethodid");
 
-        createUserAuthRecord(data, operation);
+                final XnatAuthenticationProvider provider = _manager.getProvider(authMethod, providerId);
+                if (provider != null) {
+                    data.getParameters().add("authMethod", authMethod);
+                    data.getParameters().add("providerId", providerId);
+                    data.getParameters().add("providerAutoEnabled", Boolean.toString(provider.isAutoEnabled()));
+                    data.getParameters().add("providerAutoVerified", Boolean.toString(provider.isAutoVerified()));
+                }
+                super.doPerform(data, context);
+            } catch (Exception e) {
+                retryAuthentication(data, context, "An error occurred trying to register your user account. Please try again or contact the system administrator if you need more help.");
+            }
+        }
     }
 
     @Override
     public void directRequest(final RunData data, final Context context, final UserI user) throws Exception {
         super.directRequest(data, context, user);
+    }
+
+    private void validateXnatLogin(final String username, final String password) {
+        // If it's a merge with an existing account, we need to validate the username and password.
+        if (StringUtils.isBlank(username)) {
+            throw new AuthenticationCredentialsNotFoundException("You must provide a valid username.");
+        }
+        if (StringUtils.equalsIgnoreCase("guest", username)) {
+            throw new AuthenticationCredentialsNotFoundException("You can't associate an external authentication account with the guest user.");
+        }
+        if (StringUtils.isBlank(password)) {
+            throw new AuthenticationCredentialsNotFoundException("You must provide a password value.");
+        }
+
+        try {
+            final Authentication authentication = _provider.authenticate(new XnatDatabaseUsernamePasswordAuthenticationToken(username, password));
+            if (authentication.isAuthenticated()) {
+                return;
+            }
+        } catch (AuthenticationException ignored) {
+            // We'll throw an exception below whether auth operation returned false or threw auth exception.
+        }
+        throw new BadCredentialsException("The submitted username and password didn't match an existing active XNAT account.");
     }
 
     private void retryAuthentication(final RunData data, final Context context, final String message) {
