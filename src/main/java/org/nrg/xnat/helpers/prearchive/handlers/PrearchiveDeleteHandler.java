0b678ad69e0357cf886014352b0f560b0c69535a
Mike McKay <mfmckay@wustl.edu>
2018-09-25 13:39:53 -0500

    XNAT-5368 Made study routings and remappings get cleared on study archive or deletion.

--- src/main/java/org/nrg/xnat/helpers/prearchive/handlers/PrearchiveDeleteHandler.java
+++ src/main/java/org/nrg/xnat/helpers/prearchive/handlers/PrearchiveDeleteHandler.java
@@ -9,8 +9,14 @@
 
 package org.nrg.xnat.helpers.prearchive.handlers;
 
+import org.apache.commons.lang3.StringUtils;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.services.StudyRoutingService;
+import org.nrg.xdat.turbine.utils.AdminUtils;
+import org.nrg.xnat.helpers.merge.anonymize.DefaultAnonUtils;
 import org.nrg.xnat.helpers.prearchive.PrearcDatabase;
 import org.nrg.xnat.helpers.prearchive.PrearcUtils;
+import org.nrg.xnat.helpers.prearchive.SessionData;
 import org.nrg.xnat.services.messaging.prearchive.PrearchiveOperationRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,14 +30,38 @@ public class PrearchiveDeleteHandler extends AbstractPrearchiveOperationHandler 
 
     @Override
     public void execute() throws Exception {
+        SessionData sessionData = getSessionData();
+        String studyInstanceUid = null;
+        if(sessionData!=null){
+            studyInstanceUid = sessionData.getTag();
+        }
         if (!getSessionDir().getParentFile().exists()) {
-            PrearcDatabase.unsafeSetStatus(getSessionData().getFolderName(), getSessionData().getTimestamp(), getSessionData().getProject(), PrearcUtils.PrearcStatus._DELETING);
-            PrearcDatabase.deleteCacheRow(getSessionData().getFolderName(), getSessionData().getTimestamp(), getSessionData().getProject());
+            PrearcDatabase.unsafeSetStatus(sessionData.getFolderName(), sessionData.getTimestamp(), sessionData.getProject(), PrearcUtils.PrearcStatus._DELETING);
+            PrearcDatabase.deleteCacheRow(sessionData.getFolderName(), sessionData.getTimestamp(), sessionData.getProject());
         }
         if (_log.isDebugEnabled()) {
-            _log.debug("Deleting session {} from project {}", getSessionData().getFolderName(), getSessionData().getProject());
+            _log.debug("Deleting session {} from project {}", sessionData.getFolderName(), sessionData.getProject());
         }
-        PrearcDatabase.deleteSession(getSessionData().getFolderName(), getSessionData().getTimestamp(), getSessionData().getProject());
+        PrearcDatabase.deleteSession(sessionData.getFolderName(), sessionData.getTimestamp(), sessionData.getProject());
+
+        //Clear study routing/remapping
+        if(StringUtils.isNotBlank(studyInstanceUid)){
+            try {
+                XDAT.getContextService().getBean(StudyRoutingService.class).close(studyInstanceUid);
+            }
+            catch(Exception e){
+                _log.error("Error when clearing study routing information.",e);
+            }
+            try {
+                String script = DefaultAnonUtils.getService().getStudyScript(studyInstanceUid);
+                if (StringUtils.isNotBlank(script)) {
+                    DefaultAnonUtils.getService().disableStudy(AdminUtils.getAdminUser().getLogin(), studyInstanceUid);
+                }
+            }
+            catch(Exception e){
+                _log.error("Error when clearing study remapping information.",e);
+            }
+        }
     }
 
     private static final Logger _log = LoggerFactory.getLogger(PrearchiveDeleteHandler.class);
