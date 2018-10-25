72091de15b0396459c94a27c0e1599af331b782b
Rick Herrick <jrherrick@wustl.edu>
2018-10-25 16:40:02 -0500

    XNAT-5867 Fixed bug in column ordering when building element access joins.

--- src/main/resources/META-INF/xnat/data-type-access-functions.sql
+++ src/main/resources/META-INF/xnat/data-type-access-functions.sql
@@ -1,5 +1,19 @@
 CREATE OR REPLACE VIEW public.data_type_views_element_access AS
-  SELECT coalesce(g.id, 'user:' || u.login) AS entity, a.element_name AS element_name, m.field_value, m.field, m.active_element, m.read_element, m.edit_element, m.create_element, m.delete_element, m.comparison_type, s.method, a.xdat_element_access_id AS xdat_element_access_id, s.xdat_field_mapping_set_id AS xdat_field_mapping_set_id, m.xdat_field_mapping_id AS xdat_field_mapping_id
+  SELECT
+    coalesce(g.id, 'user:' || u.login) AS entity,
+    a.element_name,
+    m.field_value,
+    m.field,
+    m.active_element,
+    m.read_element,
+    m.edit_element,
+    m.create_element,
+    m.delete_element,
+    m.comparison_type,
+    s.method,
+    a.xdat_element_access_id,
+    s.xdat_field_mapping_set_id,
+    m.xdat_field_mapping_id
   FROM
     xdat_element_access a
     LEFT JOIN xdat_user u ON a.xdat_user_xdat_user_id = u.xdat_user_id
@@ -36,14 +50,14 @@ CREATE OR REPLACE VIEW public.data_type_views_missing_mapping_elements AS
                                                                                           public_project_access_mappings));
 
 CREATE OR REPLACE VIEW public.data_type_views_orphaned_field_sets AS
-SELECT s.xdat_field_mapping_set_id AS id
-FROM
-     xdat_element_access a
-       LEFT JOIN xdat_field_mapping_set s ON a.xdat_element_access_id = s.permissions_allow_set_xdat_elem_xdat_element_access_id
-       LEFT JOIN xdat_field_mapping m ON s.xdat_field_mapping_set_id = m.xdat_field_mapping_set_xdat_field_mapping_set_id
-WHERE
-  s.xdat_field_mapping_set_id IS NOT NULL AND
-  m.xdat_field_mapping_id IS NULL;
+  SELECT s.xdat_field_mapping_set_id AS id
+  FROM
+    xdat_element_access a
+    LEFT JOIN xdat_field_mapping_set s ON a.xdat_element_access_id = s.permissions_allow_set_xdat_elem_xdat_element_access_id
+    LEFT JOIN xdat_field_mapping m ON s.xdat_field_mapping_set_id = m.xdat_field_mapping_set_xdat_field_mapping_set_id
+  WHERE
+    s.xdat_field_mapping_set_id IS NOT NULL AND
+    m.xdat_field_mapping_id IS NULL;
 
 CREATE OR REPLACE FUNCTION create_public_element_access_for_data_type(elementName VARCHAR(255))
   RETURNS BOOLEAN
@@ -127,7 +141,7 @@ DECLARE
   has_public_projects BOOLEAN;
 BEGIN
   INSERT INTO xdat_element_access (element_name, xdat_usergroup_xdat_usergroup_id)
-  SELECT elementName AS element_name, xdat_userGroup_id
+  SELECT elementName AS element_name, xdat_usergroup_id
   FROM
     xdat_usergroup g
     LEFT JOIN xdat_element_access a ON g.xdat_usergroup_id = a.xdat_usergroup_xdat_usergroup_id AND a.element_name = elementName
@@ -144,7 +158,7 @@ BEGIN
     a.element_name = elementName AND
     s.method IS NULL;
 
-  INSERT INTO xdat_field_mapping (field, field_value, create_element, read_element, edit_element, delete_element, active_element, comparison_type, xdat_field_mapping_set_xdat_field_mapping_set_id)
+  INSERT INTO xdat_field_mapping (field_value, field, create_element, read_element, edit_element, delete_element, active_element, comparison_type, xdat_field_mapping_set_xdat_field_mapping_set_id)
   SELECT field_value, field, CASE WHEN is_shared THEN create_shared ELSE create_element END AS create_element, CASE WHEN is_shared THEN read_shared ELSE read_element END AS read_element, CASE WHEN is_shared THEN edit_shared ELSE edit_element END AS edit_element, CASE WHEN is_shared THEN delete_shared ELSE delete_element END AS delete_element, CASE WHEN is_shared THEN active_shared ELSE active_element END AS active_element, comparison_type, xdat_field_mapping_set_id
   FROM
     (WITH
@@ -202,7 +216,7 @@ BEGIN
     WHERE
       e.field_value IS NOT NULL
     UNION
-    SELECT field, a.id, 0, 1, 0, 0, 1, 'equals', xdat_field_mapping_set_id
+    SELECT e.field, a.id, 0, 1, 0, 0, 1, 'equals', e.xdat_field_mapping_set_id
     FROM
       data_type_views_missing_mapping_elements e,
       project_access a
@@ -218,21 +232,21 @@ CREATE OR REPLACE FUNCTION fix_mismatched_data_type_permissions()
   RETURNS BOOLEAN
 LANGUAGE plpgsql
 AS $_$
-  DECLARE
-    has_mismatches BOOLEAN;
-    has_missing BOOLEAN;
-    data_type VARCHAR(255);
-  BEGIN
+DECLARE
+  has_mismatches BOOLEAN;
+  has_missing    BOOLEAN;
+  data_type      VARCHAR(255);
+BEGIN
     SELECT count(*) > 0 INTO has_mismatches FROM data_type_views_mismatched_mapping_elements;
     SELECT count(*) > 0 INTO has_missing FROM data_type_views_missing_mapping_elements;
     IF has_mismatches OR has_missing THEN
       DELETE FROM xdat_field_mapping WHERE xdat_field_mapping_id IN (SELECT id FROM data_type_views_mismatched_mapping_elements);
       DELETE FROM xdat_field_mapping_set WHERE xdat_field_mapping_set_id IN (SELECT id FROM data_type_views_orphaned_field_sets);
       FOR data_type IN SELECT DISTINCT primary_security_fields_primary_element_name AS data_type FROM xdat_primary_security_field WHERE primary_security_fields_primary_element_name NOT IN (SELECT DISTINCT element_name FROM xdat_element_access)
-      LOOP
-        PERFORM create_new_data_type_permissions(data_type);
-      END LOOP;
-    END IF;
+    LOOP
+      PERFORM create_new_data_type_permissions(data_type);
+    END LOOP;
+  END IF;
     RETURN has_mismatches OR has_missing;
-  END
+END
 $_$;
