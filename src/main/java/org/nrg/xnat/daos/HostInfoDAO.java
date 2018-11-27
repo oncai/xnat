1786a64817722c5dbc5c3bb329342e6dbe4b69b0
James Ransford <ransfordj@radiologics.com>
2018-11-27 12:52:59 -0600

    Make IDGenerator changes fail safe to InetAddress errors

--- src/main/java/org/nrg/xnat/daos/HostInfoDAO.java
+++ src/main/java/org/nrg/xnat/daos/HostInfoDAO.java
@@ -14,11 +14,13 @@ import java.net.UnknownHostException;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import org.nrg.dcm.id.DbBackedProjectIdentifier;
 import org.nrg.framework.orm.hibernate.AbstractHibernateDAO;
 import org.nrg.xnat.entities.HostInfo;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Repository
 public class HostInfoDAO extends AbstractHibernateDAO<HostInfo> {
 	 
@@ -56,8 +58,8 @@ public class HostInfoDAO extends AbstractHibernateDAO<HostInfo> {
 		try {
 			return getHostNumber(InetAddress.getLocalHost().getHostName());
 		} catch (UnknownHostException e) {
-			return "";
+			LoggerFactory.getLogger(HostInfoDAO.class).error("",e);
+			return "0";
 		}
 	}
-
 }
