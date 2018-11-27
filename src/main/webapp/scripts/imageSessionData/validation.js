2b107063c3add08bc50b25955b4f704abc31d0e9
James Ransford <ransfordj@radiologics.com>
2018-11-27 16:28:24 -0600

    Fix bug in validation.js prevented execution when date wasn't present

--- src/main/webapp/scripts/imageSessionData/validation.js
+++ src/main/webapp/scripts/imageSessionData/validation.js
@@ -109,7 +109,7 @@ function validateDate(){
 
 function getValueById(id){
 	var box=document.getElementById(id);
-	if(box==null){ return undefined; }
+	if(box==null){ return null; }
 	
 	if(box.value==undefined){
 		if(box.selectedIndex!=undefined){
