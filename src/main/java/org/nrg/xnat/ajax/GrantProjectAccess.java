3cd200728db7cc930c1d76e6cdf23c3ffa78615d
Mike McKay <mfmckay@wustl.edu>
2019-02-26 17:25:50 -0600

    XXX-112

--- src/main/java/org/nrg/xnat/ajax/GrantProjectAccess.java
+++ src/main/java/org/nrg/xnat/ajax/GrantProjectAccess.java
@@ -28,46 +28,61 @@ import org.nrg.xdat.turbine.utils.AdminUtils;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
 import org.nrg.xft.security.UserI;
 import org.nrg.xnat.turbine.utils.ProjectAccessRequest;
+import org.nrg.xdat.security.helpers.Users;
+import org.nrg.xdat.security.helpers.Permissions;
+import org.nrg.xdat.security.helpers.Roles;
 
 public class GrantProjectAccess {
-	public void invite(HttpServletRequest req, HttpServletResponse response,ServletConfig sc) throws IOException{
-		String email = (String)req.getParameter("email");
-		String projectID = (String)req.getParameter("project");
-		String level = (String)req.getParameter("level");
-		
-		RunDataService rundataService = null;
-        rundataService = TurbineRunDataFacade.getService();
-        try {
-            if (rundataService == null)
-            {
-                throw new TurbineException(
-                        "No RunData Service configured!");
-            }
-            RunData data = rundataService.getRunData(req, response, sc);
-            Context context = TurbineVelocity.getContext(data);
-            UserI user = XDAT.getUserDetails();
-    		
-    		XnatProjectdata project = XnatProjectdata.getXnatProjectdatasById(projectID, user, false);
-    		
-    		context.put("user",user);
-            context.put("server",TurbineUtils.GetFullServerPath());
-            context.put("siteLogoPath", XDAT.getSiteLogoPath());
-            context.put("process","Transfer to the archive.");
-            context.put("system",TurbineUtils.GetSystemName());
-            context.put("access_level",level);
-            context.put("admin_email",XDAT.getSiteConfigPreferences().getAdminEmail());
-            context.put("projectOM",project);
-            //SEND email to user
-	    ProjectAccessRequest.InviteUser(context, email, user, user.getFirstname() + " " + user.getLastname()
-		    + " has invited you to join the " + project.getName() + " "
-		    + DisplayManager.GetInstance().getSingularDisplayNameForProject().toLowerCase() + ".");
-        }catch(Exception e){
-        	
+	public void invite(HttpServletRequest req, HttpServletResponse response,ServletConfig sc) throws Exception{
+        String email = (String)req.getParameter("email");
+        String projectID = (String)req.getParameter("project");
+        String level = (String)req.getParameter("level");
+
+        String requestingUser = req.getRemoteUser();
+        UserI userObject = Users.getUser(requestingUser);
+
+
+        if (!(Permissions.canDelete(userObject, "xnat:subjectData/project", projectID) || Roles.isSiteAdmin(userObject))) {
+            //The requesting user does not have sufficient rights to add this user to this project.
+            response.setStatus(403);
+            response.setContentType("text/html");
+            response.setHeader("Cache-Control", "no-cache");
+            response.getWriter().write("RED");
         }
-		
-        response.setContentType("text/html");
-        response.setHeader("Cache-Control", "no-cache");
-        response.getWriter().write("GREEN");
+		else {
+            RunDataService rundataService = null;
+            rundataService = TurbineRunDataFacade.getService();
+            try {
+                if (rundataService == null) {
+                    throw new TurbineException(
+                            "No RunData Service configured!");
+                }
+                RunData data = rundataService.getRunData(req, response, sc);
+                Context context = TurbineVelocity.getContext(data);
+                UserI user = XDAT.getUserDetails();
+
+                XnatProjectdata project = XnatProjectdata.getXnatProjectdatasById(projectID, user, false);
+
+                context.put("user", user);
+                context.put("server", TurbineUtils.GetFullServerPath());
+                context.put("siteLogoPath", XDAT.getSiteLogoPath());
+                context.put("process", "Transfer to the archive.");
+                context.put("system", TurbineUtils.GetSystemName());
+                context.put("access_level", level);
+                context.put("admin_email", XDAT.getSiteConfigPreferences().getAdminEmail());
+                context.put("projectOM", project);
+                //SEND email to user
+                ProjectAccessRequest.InviteUser(context, email, user, user.getFirstname() + " " + user.getLastname()
+                        + " has invited you to join the " + project.getName() + " "
+                        + DisplayManager.GetInstance().getSingularDisplayNameForProject().toLowerCase() + ".");
+            } catch (Exception e) {
+
+            }
+
+            response.setContentType("text/html");
+            response.setHeader("Cache-Control", "no-cache");
+            response.getWriter().write("GREEN");
+        }
         
     }
 }
