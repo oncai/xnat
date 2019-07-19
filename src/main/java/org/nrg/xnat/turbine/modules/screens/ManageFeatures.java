b579c7194fb95a3b353f0975f8de2eabf9ed8d47
James Ransford <ransfordj@radiologics.com>
2019-07-19 15:25:08 -0500

    ManageFeatures.vm should be an Admin Only screen

--- src/main/java/org/nrg/xnat/turbine/modules/screens/ManageFeatures.java
+++ src/main/java/org/nrg/xnat/turbine/modules/screens/ManageFeatures.java
@@ -6,14 +6,14 @@ import org.apache.turbine.util.RunData;
 
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.security.helpers.Features;
-import org.nrg.xdat.turbine.modules.screens.SecureScreen;
+import org.nrg.xdat.turbine.modules.screens.AdminScreen;
 
 /**
  * @author tim@radiologics.com
  *
  * Prep class for the interactive dashboard (Quick View).  Doesn't do anything but basic initialization (security)
  */
-public class ManageFeatures extends SecureScreen {
+public class ManageFeatures extends AdminScreen {
 
 	@Override
 	protected void doBuildTemplate(RunData data, Context context) throws Exception {
