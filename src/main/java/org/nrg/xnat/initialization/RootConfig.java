62fc6a676d981eb46c5be0a7232e352c9e0d45cd
Rick Herrick <jrherrick@wustl.edu>
2019-07-29 16:20:53 -0500

    Updated for 1.7.5.4 release

--- src/main/java/org/nrg/xnat/initialization/RootConfig.java
+++ src/main/java/org/nrg/xnat/initialization/RootConfig.java
@@ -9,22 +9,17 @@
 
 package org.nrg.xnat.initialization;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.PrettyPrinter;
 import com.fasterxml.jackson.core.util.DefaultIndenter;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
-import org.nrg.framework.beans.Beans;
 import org.nrg.framework.beans.XnatPluginBeanManager;
+import org.nrg.framework.configuration.SerializerConfig;
 import org.nrg.framework.datacache.SerializerRegistry;
-import org.nrg.framework.exceptions.NrgServiceException;
 import org.nrg.framework.services.ContextService;
 import org.nrg.framework.services.SerializerService;
 import org.nrg.xdat.preferences.SiteConfigPreferences;
@@ -38,13 +33,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -57,7 +50,7 @@ import static lombok.AccessLevel.PRIVATE;
  * for standard XNAT components should be added in the {@link ApplicationConfig application configuration class}.
  */
 @Configuration
-@Import({PropertiesConfig.class, DatabaseConfig.class, SecurityConfig.class, ApplicationConfig.class, NodeConfig.class})
+@Import({PropertiesConfig.class, DatabaseConfig.class, SecurityConfig.class, ApplicationConfig.class, NodeConfig.class, SerializerConfig.class})
 @Getter(PRIVATE)
 @Accessors(prefix = "_")
 @Slf4j
@@ -104,34 +97,8 @@ public class RootConfig {
     }
 
     @Bean
-    public Jackson2ObjectMapperBuilder objectMapperBuilder() throws NrgServiceException {
-        return new Jackson2ObjectMapperBuilder()
-                .serializationInclusion(JsonInclude.Include.NON_NULL)
-                .failOnEmptyBeans(false)
-                .mixIns(mixIns())
-                .featuresToEnable(JsonParser.Feature.ALLOW_SINGLE_QUOTES, JsonParser.Feature.ALLOW_YAML_COMMENTS)
-                .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS, SerializationFeature.WRITE_NULL_MAP_VALUES)
-                .modulesToInstall(_jacksonModules);
-    }
-
-    @Bean
-    public Module hibernateModule() {
-        return new Hibernate4Module();
-    }
-
-    @Bean
     public Module guavaModule() {
         return new GuavaModule();
-    }
-
-    @Bean
-    public Map<Class<?>, Class<?>> mixIns() throws NrgServiceException {
-        return Beans.getMixIns();
-    }
-
-    @Bean
-    public SerializerService serializerService(final Jackson2ObjectMapperBuilder objectMapperBuilder) {
-        return new SerializerService(objectMapperBuilder);
     }
 
     @Bean
