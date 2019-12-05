e4f28c0dd4d50510499ae33aeca89df8e05bec6f
Rick Herrick <jrherrick@wustl.edu>
2019-12-05 17:28:24 -0600

    XNAT-5719 Added retry/timeout option for starting DICOM SCP

--- src/main/java/org/nrg/xnat/utils/NetUtils.java
+++ src/main/java/org/nrg/xnat/utils/NetUtils.java
@@ -9,20 +9,46 @@
 
 package org.nrg.xnat.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 public class NetUtils {
-    public static boolean isPortAvailable(int port) {
-        try (final ServerSocket serverSocket = new ServerSocket(port)) {
-            serverSocket.setReuseAddress(true);
-            try (final DatagramSocket datagramSocket = new DatagramSocket(port)) {
-                datagramSocket.setReuseAddress(true);
+    @SuppressWarnings("unused")
+    public static boolean isPortAvailable(final int port) {
+        return isPortAvailable(port, 0);
+    }
+
+    public static boolean isPortAvailable(final int port, final int attempts) {
+        return isPortAvailable(port, attempts, 0);
+    }
+
+    public static boolean isPortAvailable(final int port, final int attempts, final int timeout) {
+        final int resolvedAttempts = attempts == 0 ? 3 : attempts;
+        final int resolvedTimeout  = (timeout > 0 ? timeout * 2 : 1) * 500;
+        log.debug("Preparing to check port {} up to {} times if necessary, with a timeout of {} ms between each attempt", port, resolvedAttempts, resolvedTimeout);
+
+        final AtomicInteger count = new AtomicInteger();
+        while (count.getAndIncrement() < resolvedAttempts) {
+            try (final ServerSocket serverSocket = new ServerSocket(port)) {
+                serverSocket.setReuseAddress(true);
+                try (final DatagramSocket socket = new DatagramSocket(port)) {
+                    socket.setReuseAddress(true);
+                }
+                return true;
+            } catch (IOException e) {
+                log.debug("Got an exception of type \"{}\" when creating new ServerSocket instance on port {}, throwing it upstairs for handling.", e.getClass().getName(), port);
+                try {
+                    Thread.sleep(resolvedTimeout);
+                } catch (InterruptedException ex) {
+                    log.warn("Got interrupted while trying to wait for the requested port {} to become available", port, ex);
+                }
             }
-            return true;
-        } catch (IOException ignored) {
         }
         return false;
     }
