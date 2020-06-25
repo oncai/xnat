ecc3aa56101bf8bbd38da325aa6a328d97b01ed1
James Ransford <ransfordj@radiologics.com>
2020-06-25 17:51:05 -0500

    XNAT-6367: Added CustomEvent polyfill

--- src/main/webapp/scripts/polyfills.js
+++ src/main/webapp/scripts/polyfills.js
@@ -610,3 +610,18 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+/* https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent */
+if (typeof window.CustomEvent !== "function" ){
+    function CustomEvent ( event, params ) {
+        params = params || { bubbles: false, cancelable: false, detail: null };
+        var evt = document.createEvent( 'CustomEvent' );
+        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+        return evt;
+    }
+    window.CustomEvent = CustomEvent;
+}
+
+if (typeof Event !== 'function') {
+    window.Event = CustomEvent;
+}
