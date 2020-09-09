de33c05374c21cfd93766aa874f72da866478e70
Kate Alpert <kate@radiologics.com>
2020-09-09 17:46:13 -0600

    XNAT-6450: move DICOM identifier (org.nrg.dcm.id) classes to dicom-xnat-mx for consistency, updates to use config service settings across all upload avenues

--- src/main/java/org/nrg/dcm/id/ClassicDicomObjectIdentifier.java
+++ src/main/java/org/nrg/dcm/id/ClassicDicomObjectIdentifier.java
@@ -9,50 +9,11 @@
 
 package org.nrg.dcm.id;
 
-import com.google.common.collect.ImmutableList;
-import org.dcm4che2.data.Tag;
-import org.nrg.dcm.ContainedAssignmentExtractor;
-import org.nrg.dcm.Extractor;
-import org.nrg.dcm.TextExtractor;
 import org.nrg.xdat.security.user.XnatUserProvider;
 import org.nrg.xnat.services.cache.UserProjectCache;
 
-import java.util.List;
-import java.util.regex.Pattern;
-
-@SuppressWarnings("WeakerAccess")
-public class ClassicDicomObjectIdentifier extends CompositeDicomObjectIdentifier {
-    private static final ImmutableList<Extractor> attributeExtractors = new ImmutableList.Builder<Extractor>().add(new ContainedAssignmentExtractor(Tag.PatientComments, "AA", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.StudyComments, "AA", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.AdditionalPatientHistory, "AA", Pattern.CASE_INSENSITIVE))
-                                                                                                              .build();
-    private static final ImmutableList<Extractor> sessionExtractors   = new ImmutableList.Builder<Extractor>().add(new ContainedAssignmentExtractor(Tag.PatientComments, "Session", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.StudyComments, "Session", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.AdditionalPatientHistory, "Session", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new TextExtractor(Tag.PatientID))
-                                                                                                              .build();
-    private static final ImmutableList<Extractor> subjectExtractors   = new ImmutableList.Builder<Extractor>().add(new ContainedAssignmentExtractor(Tag.PatientComments, "Subject", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.StudyComments, "Subject", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new ContainedAssignmentExtractor(Tag.AdditionalPatientHistory, "Subject", Pattern.CASE_INSENSITIVE))
-                                                                                                              .add(new TextExtractor(Tag.PatientName))
-                                                                                                              .build();
-
+public class ClassicDicomObjectIdentifier extends XnatDefaultDicomObjectIdentifier {
     public ClassicDicomObjectIdentifier(final String name, final XnatUserProvider userProvider, final UserProjectCache userProjectCache) {
-        super(name, new Xnat15DicomProjectIdentifier(userProjectCache), subjectExtractors, sessionExtractors, attributeExtractors);
-        setUserProvider(userProvider);
-    }
-
-    public static List<Extractor> getAAExtractors() {
-        return attributeExtractors;
-    }
-
-    @SuppressWarnings("unused")
-    public static List<Extractor> getSessionExtractors() {
-        return sessionExtractors;
-    }
-
-    @SuppressWarnings("unused")
-    public static List<Extractor> getSubjectExtractors() {
-        return subjectExtractors;
+        super(name, userProvider, new Xnat15DicomProjectIdentifier(userProjectCache));
     }
 }
--- src/main/java/org/nrg/dcm/id/Xnat15DicomProjectIdentifier.java
+++ src/main/java/org/nrg/dcm/id/Xnat15DicomProjectIdentifier.java
@@ -11,7 +11,9 @@ package org.nrg.dcm.id;
 
 import lombok.extern.slf4j.Slf4j;
 import org.dcm4che2.data.Tag;
-import org.nrg.config.entities.Configuration;
+import org.nrg.dcm.ContainedAssignmentExtractor;
+import org.nrg.dcm.Extractor;
+import org.nrg.dcm.TextExtractor;
 import org.nrg.framework.configuration.ConfigPaths;
 import org.nrg.xdat.XDAT;
 import org.nrg.xft.XFT;
@@ -23,78 +25,52 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
 public class Xnat15DicomProjectIdentifier extends DbBackedProjectIdentifier {
-    public static final String DICOM_ROUTING_TOOL        = "dicom";
-    public static final String PROJECT_ROUTING_RULES_CFG = "projectRules";
-
     public Xnat15DicomProjectIdentifier(final UserProjectCache userProjectCache) {
         super(userProjectCache);
     }
 
-    protected List<DicomDerivedString> getIdentifiers() {
-        final List<DicomDerivedString> identifiers = Arrays.asList(new ContainedAssignmentDicomIdentifier(Tag.PatientComments, PROJECT, Pattern.CASE_INSENSITIVE),
-                                                                   new ContainedAssignmentDicomIdentifier(Tag.StudyComments, PROJECT, Pattern.CASE_INSENSITIVE),
-                                                                   new ContainedAssignmentDicomIdentifier(Tag.AdditionalPatientHistory, PROJECT, Pattern.CASE_INSENSITIVE),
-                                                                   new TextDicomIdentifier(Tag.StudyDescription),
-                                                                   new TextDicomIdentifier(Tag.AccessionNumber));
+    @Override
+    protected List<Extractor> getIdentifiers() {
+        final List<Extractor> identifiers = Arrays.asList(new ContainedAssignmentExtractor(Tag.PatientComments, PROJECT, Pattern.CASE_INSENSITIVE),
+                new ContainedAssignmentExtractor(Tag.StudyComments, PROJECT, Pattern.CASE_INSENSITIVE),
+                new ContainedAssignmentExtractor(Tag.AdditionalPatientHistory, PROJECT, Pattern.CASE_INSENSITIVE),
+                new TextExtractor(Tag.StudyDescription),
+                new TextExtractor(Tag.AccessionNumber));
         loadFrom15Config(identifiers);
         return identifiers;
     }
 
-    private static void loadFrom15Config(final Collection<DicomDerivedString> identifiers) {
+    @Override
+    protected List<Extractor> getDynamicExtractors() {
+        return XnatDefaultDicomObjectIdentifier.getExtractorsFromConfig(CompositeDicomObjectIdentifier.ExtractorType.PROJECT);
+    }
+
+    private static void loadFrom15Config(final Collection<Extractor> identifiers) {
+        final ConfigPaths   paths         = XDAT.getContextService().getBeanSafely(ConfigPaths.class);
+        final Path          confDir       = Paths.get(XFT.GetConfDir());
+        if (!paths.contains(confDir)) {
+            paths.add(confDir);
+        }
+        final List<File> configs = paths.findFiles(DICOM_PROJECT_RULES);
+        if (configs.isEmpty()) {
+            return;
+        }
+        File cfgFile = configs.get(0);
+        if (configs.size() > 1) {
+            log.warn("Multiple project identifier config files found {}, using {}", configs, cfgFile);
+        }
         try {
-            final Reader        source;
-            final Configuration configuration = XDAT.getConfigService().getConfig(DICOM_ROUTING_TOOL, PROJECT_ROUTING_RULES_CFG);
-            final ConfigPaths   paths         = XDAT.getContextService().getBeanSafely(ConfigPaths.class);
-            final Path          confDir       = Paths.get(XFT.GetConfDir());
-            if (!paths.contains(confDir)) {
-                paths.add(confDir);
-            }
-            final List<File> configs = paths.findFiles(DICOM_PROJECT_RULES);
-            if (configuration != null && configuration.isEnabled() && org.apache.commons.lang.StringUtils.isNotBlank(configuration.getContents())) {
-                source = new StringReader(configuration.getContents());
-            } else if (configs.size() > 0) {
-                source = new FileReader(configs.get(0));
-            } else {
-                source = null;
-            }
-
-            if (source != null) {
-                try (final BufferedReader reader = new BufferedReader(source)) {
-                    String line;
-                    while (null != (line = reader.readLine())) {
-                        final DicomDerivedString extractor = parseRule(line);
-                        if (null != extractor) {
-                            identifiers.add(extractor);
-                        }
-                    }
-                }
-            }
-        } catch (FileNotFoundException ignored) {
-            //
+            List<Extractor> extractorsFromFile = XnatDefaultDicomObjectIdentifier.parseAsExtractors(new FileReader(cfgFile));
+            identifiers.addAll(extractorsFromFile);
         } catch (IOException e) {
-            log.error("An error occurred trying to open the DICOM project rules configuration", e);
+            log.error("An error occurred trying to parse the {} configuration", cfgFile, e);
         }
     }
 
-    private static DicomDerivedString parseRule(final String rule) {
-        final Matcher matcher = CUSTOM_RULE_PATTERN.matcher(rule);
-        if (matcher.matches()) {
-            final int    tag      = Integer.decode("0x" + matcher.group(1) + matcher.group(2));
-            final String regexp   = matcher.group(3);
-            final String groupIdx = matcher.group(4);
-            final int    group    = null == groupIdx ? 1 : Integer.parseInt(groupIdx);
-            return new PatternDicomIdentifier(tag, Pattern.compile(regexp), group);
-        } else {
-            return null;
-        }
-    }
-
-    private static final Pattern CUSTOM_RULE_PATTERN = Pattern.compile("\\((\\p{XDigit}{4}),(\\p{XDigit}{4})\\):(.+?)(?::(\\d+))?");
     private static final String  DICOM_PROJECT_RULES = "dicom-project.rules";
     private static final String  PROJECT             = "Project";
 }
