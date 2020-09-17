8afe3f5389e1f063e33f7186e76b07dbb8105be7
Will Horton <hortonw@mir.wustl.edu>
2020-09-17 15:20:38 -0500

    XNAT-6013: Remove Subscription ID column and Event ID column from history table. Add a 'Find By ID' input to the table header. Reorder columns so date is first and used as the sorting mechanism.

--- src/main/webapp/scripts/xnat/admin/eventServiceUi.js
+++ src/main/webapp/scripts/xnat/admin/eventServiceUi.js
@@ -1005,7 +1005,7 @@ var XNAT = getObject(XNAT || {});
         var datatableId = $datatable.prop('id');
         $datatable.find('thead').append('<tr id="' + filterHeaderRowId + '" class="filter">');
 
-        dataTableColumns.forEach(function(column){
+        dataTableColumns.forEach(function(column,i){
             if (column.mData) {
                 var inputId = filterHeaderRowId + "Input" + i;
                 jq("#" + filterHeaderRowId).append('<th class="noPointer"><input type="text" id="' + inputId + '" name="' + inputId + '" placeholder="Filter..." class="filter_init datatable-filter" /></th>');
@@ -1064,13 +1064,33 @@ var XNAT = getObject(XNAT || {});
         var datatableOptions = {
             aaData: data,
             aoColumns: [
+                // {
+                //     sTitle: '<b>Subscription ID</b>',
+                //     sClass: 'left',
+                //     sWidth: '80px',
+                //     mData: function(source){
+                //         return source['subscription-id']
+                //     }
+                // },
                 {
-                    sTitle: '<b>Subscription ID</b>',
-                    sClass: 'left',
-                    sWidth: '80px',
+                    sTitle: '<b>Date</b>',
                     mData: function(source){
-                        return source['subscription-id']
-                    }
+                        var timestamp  = source.timestamp || '';
+                        var dateString = '';
+                        if (timestamp) {
+                            timestamp = timestamp.replace(/-/g, '/'); // include date format hack for Safari
+                            if (timestamp.indexOf('UTC') < 0) {
+                                timestamp = timestamp.trim() + ' UTC';
+                            }
+                            dateString = (new Date(timestamp)).toLocaleString();
+                            // dateString = timestamp.toISOString().replace('T',' ').replace('Z',' ').split('.')[0];
+
+                        } else {
+                            dateString = 'N/A';
+                        }
+                        return dateString
+                    },
+                    sWidth: '150px'
                 },
                 {
                     sTitle: '<b>Subscription Name</b>',
@@ -1110,26 +1130,6 @@ var XNAT = getObject(XNAT || {});
                         return source.project
                     },
                     sWidth: '150px'
-                },
-                {
-                    sTitle: '<b>Date</b>',
-                    mData: function(source){
-                        var timestamp  = source.timestamp || '';
-                        var dateString = '';
-                        if (timestamp) {
-                            timestamp = timestamp.replace(/-/g, '/'); // include date format hack for Safari
-                            if (timestamp.indexOf('UTC') < 0) {
-                                timestamp = timestamp.trim() + ' UTC';
-                            }
-                            dateString = (new Date(timestamp)).toLocaleString();
-                            // dateString = timestamp.toISOString().replace('T',' ').replace('Z',' ').split('.')[0];
-
-                        } else {
-                            dateString = 'N/A';
-                        }
-                        return dateString
-                    },
-                    sWidth: '150px'
                 }
             ],
             iDisplayLength: dataLengthToDisplay,
@@ -1149,9 +1149,8 @@ var XNAT = getObject(XNAT || {});
         addColumnFilters($datatable,datatableOptions.aoColumns);
     };
 
-
     function historyItemErrorDialog(id){
-        console.error('Error displaying history item width id: ' + id);
+        console.error('Error displaying history item with id: ' + id);
         XNAT.ui.dialog.open({
             content: 'Sorry, could not display this history item.',
             buttons: [
@@ -1163,7 +1162,6 @@ var XNAT = getObject(XNAT || {});
             ]
         });
     }
-
 
     historyTable.viewHistory = function(id){
 
@@ -1264,8 +1262,24 @@ var XNAT = getObject(XNAT || {});
         if (historyEntry) historyTable.viewHistory(historyEntry);
     });
 
+    historyTable.findById = function(e){
+        e.preventDefault();
+        var validIds = Object.keys(XNAT.admin.eventServicePanel.historyData),
+            submittedId = $('#event-id-entry').val();
+        if (submittedId && validIds.indexOf(submittedId) >= 0) {
+            XNAT.admin.eventServicePanel.historyTable.viewHistory(submittedId);
+            $('#event-id-entry').val('');
+            return;
+        }
+        else {
+            XNAT.ui.dialog.message('Please enter a valid event history ID');
+            $('#event-id-entry').focus();
+        }
+    };
+
+
     historyTable.init = historyTable.refresh = function(container){
-        var $container = $$(container || '#history-table-container'), _historyTable;
+        var $container = $$(container || '#history-table-container');
 
         historyTable.getHistory().done(function(data){
             if (data.length){
@@ -1273,6 +1287,30 @@ var XNAT = getObject(XNAT || {});
                 var $datatable = $.spawn('table#event-history-table.xnat-table.data-table.compact', { style: { width: '100%' }});
                 $container.empty().append([h3, $datatable]);
                 historyTable.datatable(data, $datatable);
+
+                // add a "find by ID" input field after the table renders
+                var target = $('#event-history-table_length'),
+                    searchHistoryInput = spawn('input#event-id-entry', {
+                        type:'text',
+                        name: 'findbyid',
+                        placeholder: 'Find By ID',
+                        size: 12,
+                        style: {'font-size':'12px' }}
+                        ),
+                    searchHistoryButton = spawn(
+                        'button.btn2.btn-sm',[
+                            spawn('i.fa.fa-search',{
+                                title: 'Find By ID',
+                                onclick: XNAT.admin.eventServicePanel.historyTable.findById
+                            })
+                        ]);
+                target.append(spawn('div.pull-right',[
+                    searchHistoryInput,
+                    spacer(4),
+                    searchHistoryButton
+                ]));
+
+
             } else {
                 $container.empty().append(spawn('p','No event history to display'));
             }
