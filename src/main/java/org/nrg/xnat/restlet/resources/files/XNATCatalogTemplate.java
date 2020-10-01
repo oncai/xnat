79ab806801fdf72da08d7387f5abf79ef063cf6e
Rick Herrick <jrherrick@wustl.edu>
2020-10-01 13:42:10 -0500

    XNAT-6458 Altered query to pass resolved resource IDs through data_type_fns_can_action_entity() function rather than predicate

--- src/main/java/org/nrg/xnat/restlet/resources/files/XNATCatalogTemplate.java
+++ src/main/java/org/nrg/xnat/restlet/resources/files/XNATCatalogTemplate.java
@@ -9,7 +9,6 @@
 
 package org.nrg.xnat.restlet.resources.files;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.Value;
@@ -20,11 +19,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.nrg.action.ClientException;
 import org.nrg.xdat.om.*;
-import org.nrg.xdat.security.helpers.Permissions;
 import org.nrg.xft.XFTTable;
 import org.nrg.xft.event.EventMetaI;
 import org.nrg.xft.event.EventUtils;
-import org.nrg.xft.security.UserI;
 import org.nrg.xnat.helpers.resource.XnatResourceInfo;
 import org.nrg.xnat.helpers.resource.direct.DirectResourceModifierBuilder;
 import org.nrg.xnat.helpers.resource.direct.ResourceModifierA;
@@ -36,7 +33,6 @@ import org.restlet.data.Status;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
-import javax.annotation.Nonnull;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -59,15 +55,14 @@ public class XNATCatalogTemplate extends XNATTemplate {
             // Separate numeric and non-numeric IDs to start. Non-numeric IDs get qualified by project/experiment/etc later.
             final Map<Boolean, List<String>> separated = requestedResources.stream().collect(Collectors.partitioningBy(NumberUtils::isDigits));
             _resourceIds.addAll(separated.get(false));
-            final List<String> requestedResourceIds = separated.get(true);
+            final List<Integer> requestedResourceIds = separated.get(true).stream().map(Integer::parseInt).collect(Collectors.toList());
 
             // Add all numeric IDs that are either permitted based on associated security ID or that have no security ID (in construction)
             if (!requestedResourceIds.isEmpty()) {
-                final Predicate<ResourceMap> predicate = new PermittedResourcePredicate(getUser());
-                final List<String> resourceIds = getTemplate().query(QUERY_FIND_RESOURCE_SECURE_OBJECTS, new MapSqlParameterSource("resourceIds", requestedResourceIds), RESOURCE_MAP_ROW_MAPPER).stream().filter(predicate).map(ResourceMap::resourceIdAsString).collect(Collectors.toList());
+                final List<String> resourceIds = getTemplate().query(QUERY_FIND_RESOURCE_SECURE_OBJECTS, new MapSqlParameterSource("resourceIds", requestedResourceIds).addValue("username", getUser().getUsername()), RESOURCE_MAP_ROW_MAPPER).stream().map(ResourceMap::resourceIdAsString).collect(Collectors.toList());
                 if (resourceIds.size() < requestedResourceIds.size()) {
                     final int requested = requestedResourceIds.size();
-                    requestedResourceIds.removeAll(resourceIds);
+                    requestedResourceIds.removeAll(resourceIds.stream().map(Integer::parseInt).collect(Collectors.toList()));
                     throw new ClientException(Status.CLIENT_ERROR_FORBIDDEN, "The user " + getUser().getUsername() + " requested " + requested + " resources by ID (i.e. not by qualified resource label) but was denied access to " + (resourceIds.isEmpty() ? "all" : requestedResourceIds.size()) + " of them: " + StringUtils.join(requestedResourceIds, ", "));
                 }
                 _resourceIds.addAll(resourceIds);
@@ -178,106 +173,75 @@ public class XNATCatalogTemplate extends XNATTemplate {
         String resourceIdAsString() {
             return Long.toString(_resourceId);
         }
+
         long   _resourceId;
         String _xsiType;
         String _securityId;
         String _projectId;
     }
 
-    @Data
-    @Accessors(prefix = "_")
-    private static class PermittedResourcePredicate implements Predicate<ResourceMap> {
-        public PermittedResourcePredicate(final UserI user) {
-            _user = user;
-        }
-
-        @Override
-        public boolean test(final ResourceMap resourceMap) {
-            if (resourceMap == null) {
-                return false;
-            }
-            final String xsiType   = resourceMap.getXsiType();
-            final String projectId = resourceMap.getProjectId();
-            try {
-                if ((StringUtils.equalsIgnoreCase(xsiType, XnatProjectdata.SCHEMA_ELEMENT_NAME)
-                     ? Collections.singletonList(XnatProjectdata.SCHEMA_ELEMENT_NAME + "/ID")
-                     : Arrays.asList(xsiType + "/project", xsiType + "/sharing/share/project")).stream().anyMatch(xmlPath -> {
-                    try {
-                        return Permissions.canRead(getUser(), xmlPath, projectId);
-                    } catch (Exception e) {
-                        return false;
-                    }
-                })) {
-                    return true;
-                }
-                log.error("The user {} requested the resource with ID {} on the object {}/ID[{}] but was denied access", getUser().getUsername(), resourceMap.getResourceId(), xsiType, resourceMap.getSecurityId());
-                return false;
-            } catch (Exception e) {
-                log.error("An error occurred trying to check permissions for user {} on the object {}/ID[{}], denying by default", getUser().getUsername(), xsiType, resourceMap.getSecurityId(), e);
-                return false;
-            }
-        }
-
-        private final UserI _user;
-    }
-
-    private static final String QUERY_FIND_RESOURCE_SECURE_OBJECTS = "SELECT " +
-                                                                     "  a.xnat_abstractresource_id AS resourceId, " +
-                                                                     "  r.uri AS uri, " +
-                                                                     "  xme.element_name AS xsiType, " +
-                                                                     "  e.id AS securityId, " +
-                                                                     "  e.project AS projectId " +
+    private static final String QUERY_FIND_RESOURCE_SECURE_OBJECTS = "WITH " +
+                                                                     "    resources AS ( " +
+                                                                     "        SELECT " +
+                                                                     "            a.xnat_abstractresource_id AS resourceId, " +
+                                                                     "            r.uri                      AS uri, " +
+                                                                     "            xme.element_name           AS xsiType, " +
+                                                                     "            e.id                       AS securityId, " +
+                                                                     "            e.project                  AS projectId " +
+                                                                     "        FROM " +
+                                                                     "            xnat_abstractresource a " +
+                                                                     "                LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_imagescandata s ON a.xnat_imagescandata_xnat_imagescandata_id = s.xnat_imagescandata_id " +
+                                                                     "                LEFT JOIN img_assessor_in_resource iain ON a.xnat_abstractresource_id = iain.xnat_abstractresource_xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN img_assessor_out_resource iaout ON a.xnat_abstractresource_id = iaout.xnat_abstractresource_xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN recon_in_resource rin ON a.xnat_abstractresource_id = rin.xnat_abstractresource_xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN recon_out_resource rout ON a.xnat_abstractresource_id = rout.xnat_abstractresource_xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_reconstructedimagedata recon ON COALESCE(rin.xnat_reconstructedimagedata_xnat_reconstructedimagedata_id, rout.xnat_reconstructedimagedata_xnat_reconstructedimagedata_id) = recon.xnat_reconstructedimagedata_id " +
+                                                                     "                LEFT JOIN xnat_experimentdata_resource eres ON a.xnat_abstractresource_id = eres.xnat_abstractresource_xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_experimentdata e ON COALESCE(s.image_session_id, eres.xnat_experimentdata_id, iaout.xnat_imageassessordata_id, iain.xnat_imageassessordata_id, recon.image_session_id) = e.id " +
+                                                                     "                LEFT JOIN xdat_meta_element xme ON e.extension = xme.xdat_meta_element_id " +
+                                                                     "        WHERE " +
+                                                                     "            a.xnat_abstractresource_id IN (:resourceIds) AND " +
+                                                                     "            e.id IS NOT NULL " +
+                                                                     "        UNION " +
+                                                                     "        SELECT " +
+                                                                     "            a.xnat_abstractresource_id AS resourceId, " +
+                                                                     "            r.uri                      AS uri, " +
+                                                                     "            'xnat:subjectData'         AS xsiType, " +
+                                                                     "            s.id                       AS securityId, " +
+                                                                     "            s.project                  AS projectId " +
+                                                                     "        FROM " +
+                                                                     "            xnat_subjectdata_resource res " +
+                                                                     "                LEFT JOIN xnat_abstractresource a ON res.xnat_abstractresource_xnat_abstractresource_id = a.xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_subjectdata S ON res.xnat_subjectdata_id = S.id " +
+                                                                     "        WHERE " +
+                                                                     "            a.xnat_abstractresource_id IN (:resourceIds) " +
+                                                                     "        UNION " +
+                                                                     "        SELECT " +
+                                                                     "            a.xnat_abstractresource_id AS resourceId, " +
+                                                                     "            r.uri                      AS uri, " +
+                                                                     "            'xnat:projectData'         AS xsiType, " +
+                                                                     "            p.id                       AS securityId, " +
+                                                                     "            p.id                       AS projectId " +
+                                                                     "        FROM " +
+                                                                     "            xnat_projectdata_resource res " +
+                                                                     "                LEFT JOIN xnat_abstractresource a ON res.xnat_abstractresource_xnat_abstractresource_id = a.xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
+                                                                     "                LEFT JOIN xnat_projectdata p ON res.xnat_projectdata_id = p.id " +
+                                                                     "        WHERE " +
+                                                                     "            a.xnat_abstractresource_id IN (:resourceIds)) " +
+                                                                     "SELECT * " +
                                                                      "FROM " +
-                                                                     "  xnat_abstractresource a " +
-                                                                     "  LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_imagescandata s ON a.xnat_imagescandata_xnat_imagescandata_id = s.xnat_imagescandata_id " +
-                                                                     "  LEFT JOIN img_assessor_in_resource iain ON a.xnat_abstractresource_id = iain.xnat_abstractresource_xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN img_assessor_out_resource iaout ON a.xnat_abstractresource_id = iaout.xnat_abstractresource_xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN recon_in_resource rin ON a.xnat_abstractresource_id = rin.xnat_abstractresource_xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN recon_out_resource rout ON a.xnat_abstractresource_id = rout.xnat_abstractresource_xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_reconstructedimagedata recon ON COALESCE(rin.xnat_reconstructedimagedata_xnat_reconstructedimagedata_id, rout.xnat_reconstructedimagedata_xnat_reconstructedimagedata_id) = recon.xnat_reconstructedimagedata_id " +
-                                                                     "  LEFT JOIN xnat_experimentdata_resource eres ON a.xnat_abstractresource_id = eres.xnat_abstractresource_xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_experimentdata e ON COALESCE(s.image_session_id, eres.xnat_experimentdata_id, iaout.xnat_imageassessordata_id, iain.xnat_imageassessordata_id, recon.image_session_id) = e.id " +
-                                                                     "  LEFT JOIN xdat_meta_element xme ON e.extension = xme.xdat_meta_element_id " +
+                                                                     "    resources " +
                                                                      "WHERE " +
-                                                                     "  a.xnat_abstractresource_id::VARCHAR(64) IN (:resourceIds) AND " +
-                                                                     "  e.id IS NOT NULL " +
-                                                                     "UNION " +
-                                                                     "SELECT " +
-                                                                     "  a.xnat_abstractresource_id AS resourceId, " +
-                                                                     "  r.uri AS uri, " +
-                                                                     "  'xnat:subjectData' AS xsiType, " +
-                                                                     "  s.id AS securityId, " +
-                                                                     "  s.project AS projectId " +
-                                                                     "FROM " +
-                                                                     "  xnat_subjectdata_resource res " +
-                                                                     "  LEFT JOIN xnat_abstractresource a ON res.xnat_abstractresource_xnat_abstractresource_id = a.xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_subjectdata S ON res.xnat_subjectdata_id = S.id " +
-                                                                     "WHERE " +
-                                                                     "  a.xnat_abstractresource_id::VARCHAR(64) IN (:resourceIds) " +
-                                                                     "UNION " +
-                                                                     "SELECT " +
-                                                                     "  a.xnat_abstractresource_id AS resourceId, " +
-                                                                     "  r.uri AS uri, " +
-                                                                     "  'xnat:projectData' AS xsiType, " +
-                                                                     "  p.id AS securityId, " +
-                                                                     "  p.id AS projectId " +
-                                                                     "FROM " +
-                                                                     "  xnat_projectdata_resource res " +
-                                                                     "  LEFT JOIN xnat_abstractresource a ON res.xnat_abstractresource_xnat_abstractresource_id = a.xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_resource r ON a.xnat_abstractresource_id = r.xnat_abstractresource_id " +
-                                                                     "  LEFT JOIN xnat_projectdata p ON res.xnat_projectdata_id = p.id " +
-                                                                     "WHERE " +
-                                                                     "  a.xnat_abstractresource_id::VARCHAR(64) IN (:resourceIds)";
+                                                                     "    data_type_fns_can_action_entity(:username, 'read', securityId) = TRUE";
 
     private static final RowMapper<ResourceMap> RESOURCE_MAP_ROW_MAPPER = (resultSet, index) -> new ResourceMap(resultSet.getLong("resourceId"), resultSet.getString("xsiType"), resultSet.getString("securityId"), resultSet.getString("projectId"));
     private static final Predicate<String>      IS_A_NUMBER             = Pattern.compile("^\\d+$").asPredicate();
     private static final Predicate<String>      NOT_A_NUMBER            = IS_A_NUMBER.negate();
 
-    @Nonnull
     private final List<String>               _resourceIds = new ArrayList<>();
-    @Nonnull
     private final List<XnatAbstractresource> _resources   = new ArrayList<>();
 
     private XFTTable _catalogs = null;
