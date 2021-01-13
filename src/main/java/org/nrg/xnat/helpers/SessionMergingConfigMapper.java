632f59676b3ac09b15532807c4479708bd99cc5f
Rick Herrick <jrherrick@wustl.edu>
2021-01-13 16:04:51 -0600

    XNAT-6580 Upgraded activemq library version

--- src/main/java/org/nrg/xnat/helpers/SessionMergingConfigMapper.java
+++ src/main/java/org/nrg/xnat/helpers/SessionMergingConfigMapper.java
@@ -1,13 +1,9 @@
 package org.nrg.xnat.helpers;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang.StringUtils;
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
 import org.nrg.config.entities.Configuration;
 import org.nrg.config.services.ConfigService;
 import org.nrg.framework.exceptions.NrgServiceError;
@@ -15,47 +11,49 @@ import org.nrg.framework.exceptions.NrgServiceRuntimeException;
 import org.nrg.xdat.XDAT;
 import org.nrg.xdat.om.XnatProjectdata;
 
-public class SessionMergingConfigMapper {
-	 private  final ObjectMapper MAPPER = new ObjectMapper(new JsonFactory());
-	 private  final TypeReference<HashMap<String, String>> MAP_TYPE_REFERENCE = new TypeReference<HashMap<String, String>>() {};
-	    
-	    
-	    public Map<String, String> getSessionMergingConfigMap(Configuration configuration) {
-	        if (configuration == null) {
-	            return getSessionMergingConfigMap();
-	        }
-	        return getSessionMergingConfigMap(configuration.getContents());
-	    }
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-	    public  Map<String, String> getSessionMergingConfigMap(String contents) {
-	        if (StringUtils.isBlank(contents)) {
-	            return getSessionMergingConfigMap();
-	        }
-	        try {
-	            return MAPPER.readValue(contents, MAP_TYPE_REFERENCE);
-	        } catch (IOException exception) {
-	            throw new NrgServiceRuntimeException(NrgServiceError.Unknown, "Something went wrong unmarshalling the configuration.", exception);
-	        }
-	    }
-	    
-	    private  Map<String, String> getSessionMergingConfigMap() {
-	        Map<String, String> map = new HashMap<String, String>();
-	        map.put("enabled", "false");
-	        map.put("sessionmerging_uid_mod", "");
-	        return map;
-	    }
-	    
-	    public boolean getUidModSetting(String project)   {
-	        
-	        ConfigService configService = XDAT.getConfigService();
-	      
-	        // check project config
-	        Configuration config =configService.getConfig("sessionmerging", "script", XnatProjectdata.getProjectInfoIdFromStringId(project));
-	        if (config != null && config.getStatus().equals("enabled")) {
-	            return Boolean.parseBoolean(this.getSessionMergingConfigMap(config).get("sessionmerging_uid_mod"));
-	        }
-	        // if nothing there, check site config
-	        return XDAT.getBoolSiteConfigurationProperty("sessionmerging_uid_mod",false);
-	       
-	    }
+public class SessionMergingConfigMapper {
+    public SessionMergingConfigMapper() {
+        _configService = XDAT.getConfigService();
+    }
+
+    public Map<String, String> getSessionMergingConfigMap(final Configuration configuration) {
+        return configuration == null ? getDefaultSessionMergingConfigMap() : getSessionMergingConfigMap(configuration.getContents());
+    }
+
+    public Map<String, String> getSessionMergingConfigMap(final String contents) {
+        try {
+            return StringUtils.isBlank(contents) ? getDefaultSessionMergingConfigMap() : MAPPER.readValue(contents, MAP_TYPE_REFERENCE);
+        } catch (IOException exception) {
+            throw new NrgServiceRuntimeException(NrgServiceError.Unknown, "Something went wrong unmarshalling the configuration.", exception);
+        }
+    }
+
+    @SuppressWarnings({"BooleanMethodIsAlwaysInverted", "deprecation"})
+    public boolean getUidModSetting(final String project) {
+        // check project config
+        final Configuration config = _configService.getConfig("sessionmerging", "script", XnatProjectdata.getProjectInfoIdFromStringId(project));
+        if (config != null && StringUtils.equals(config.getStatus(), "enabled")) {
+            return Boolean.parseBoolean(getSessionMergingConfigMap(config).get("sessionmerging_uid_mod"));
+        }
+        // if nothing there, check site config
+        return XDAT.getBoolSiteConfigurationProperty("sessionmerging_uid_mod", false);
+
+    }
+
+    private Map<String, String> getDefaultSessionMergingConfigMap() {
+        final Map<String, String> map = new HashMap<>();
+        map.put("enabled", "false");
+        map.put("sessionmerging_uid_mod", "");
+        return map;
+    }
+
+    private static final ObjectMapper                           MAPPER             = new ObjectMapper(new JsonFactory());
+    private static final TypeReference<HashMap<String, String>> MAP_TYPE_REFERENCE = new TypeReference<HashMap<String, String>>() {
+    };
+
+    private final ConfigService _configService;
 }
