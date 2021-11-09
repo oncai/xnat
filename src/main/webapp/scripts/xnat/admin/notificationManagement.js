73ea68aeae8a3955a8b3fe3aec098086e3d845cf
William Horton <hortonw@wustl.edu>
2021-11-09 17:42:07 -0600

    XNAT-6890 XNAT-6886 Refactor notification panel. Restore descriptive text and make it easier to browse and save work in email panel.

---
+++ src/main/webapp/scripts/xnat/admin/notificationManagement.js
@@ -0,0 +1,66 @@
+/*
+ * web: notificationManagement.js
+ * XNAT http://www.xnat.org
+ * Copyright (c) 2005-2017, Washington University School of Medicine and Howard Hughes Medical Institute
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD.
+ */
+
+var XNAT = getObject(XNAT);
+
+(function($, console){
+
+    var notifyMgr;
+
+    console.log('notificationManagement.js');
+
+    XNAT.admin =
+        getObject(XNAT.admin||{});
+
+    XNAT.admin.notificationManager = notifyMgr =
+        getObject(XNAT.admin.notificationManager||{});
+
+    notifyMgr.templates = []
+
+    function populateNotificationSelector(){
+        notifyMgr.templates.length=0;
+
+        var container = $(document).find('#notification-email-container');
+        container.css({
+            'background':'#f0f0f0',
+            'border':'1px solid #e0e0e0',
+            'padding':'15px 15px 0'
+        });
+
+        var selector = $(document).find('#notification-email-selector');
+
+        var emailTemplates = $(container).find('.panel-element').toArray();
+        emailTemplates.forEach(function(template,i){
+            // index each of the email template editor panels, add a corresponding option to the selector, then hide them except for the first.
+            var name = $(template).data('name'),
+                label = $(template).find('.element-label').html();
+
+            notifyMgr.templates.push({
+                name: name,
+                label: label
+            });
+
+            selector.append(spawn('option',{ value: name },label));
+
+            if (i>0) $(template).hide();
+
+        });
+
+        selector.off('change').on('change',function(){
+            var name = $(this).find('option:selected').val();
+            emailTemplates.filter(function(template){ return $(template).css('display') !== 'none' })
+                .forEach(function(visibleTemplate){ $(visibleTemplate).hide() });
+            emailTemplates.filter(function(template){ return $(template).data('name') === name })
+                .forEach(function(selectedTemplate){ $(selectedTemplate).show(); });
+        })
+    }
+
+    $(populateNotificationSelector);
+
+})(window.jQuery, window.console);
