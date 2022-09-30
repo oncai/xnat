c28c02cd2b5e828bbe1d05e0fab83a1468db3e10
David Maffitt <davidmaffitt@flywheel.io>
2022-09-30 12:46:47 -0500

    XNAT-6742 DicomEdit changes for retainPrivateTags fix.

--- src/main/java/org/nrg/xnat/configuration/DicomImportConfig.java
+++ src/main/java/org/nrg/xnat/configuration/DicomImportConfig.java
@@ -13,6 +13,7 @@ import org.nrg.dcm.DicomFileNamer;
 import org.nrg.dcm.id.ClassicDicomObjectIdentifier;
 import org.nrg.dcm.id.TemplatizedDicomFileNamer;
 import org.nrg.dcm.scp.daos.DicomSCPInstanceService;
+import org.nrg.dicom.dicomedit.DE6ScriptFactory;
 import org.nrg.xdat.om.XnatProjectdata;
 import org.nrg.xdat.preferences.HandlePetMr;
 import org.nrg.xdat.preferences.SiteConfigPreferences;
@@ -39,6 +40,11 @@ public class DicomImportConfig {
                                                                         final UserProjectCache userProjectCache) {
         final String name = messageSource.getMessage("dicomConfig.defaultObjectIdentifier", new Object[]{ClassicDicomObjectIdentifier.class.getSimpleName()}, "Default DICOM object identifier ({0})", Locale.getDefault());
         return new ClassicDicomObjectIdentifier(name, receivedFileUserProvider, userProjectCache);
+    }
+
+    @Bean
+    public DE6ScriptFactory scriptFactory() {
+        return new DE6ScriptFactory();
     }
 
     @Bean
