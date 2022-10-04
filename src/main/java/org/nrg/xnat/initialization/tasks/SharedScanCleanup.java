4a261e2bfdbf7a8f42e476793c367f4c46900e95
bin.zhang <binzhang@flywheel.io>
2022-10-04 21:47:36 +0000

    XNAT-7184 Fixed steep memory pressure from orphaned scans

--- src/main/java/org/nrg/xnat/initialization/tasks/SharedScanCleanup.java
+++ src/main/java/org/nrg/xnat/initialization/tasks/SharedScanCleanup.java
@@ -1,17 +1,20 @@
 package org.nrg.xnat.initialization.tasks;
 
 import lombok.extern.slf4j.Slf4j;
-import org.nrg.xdat.model.XnatExperimentdataShareI;
-import org.nrg.xdat.model.XnatImagescandataI;
+import org.apache.commons.lang3.StringUtils;
 import org.nrg.xdat.model.XnatImagescandataShareI;
-import org.nrg.xdat.om.*;
+import org.nrg.xdat.om.WrkWorkflowdata;
+import org.nrg.xdat.om.XnatImagescandata;
+import org.nrg.xdat.om.XnatImagescandataShare;
 import org.nrg.xdat.security.helpers.Roles;
 import org.nrg.xdat.security.user.XnatUserProvider;
+import org.nrg.xft.XFTTable;
 import org.nrg.xft.event.EventDetails;
 import org.nrg.xft.event.EventMetaI;
 import org.nrg.xft.event.EventUtils;
 import org.nrg.xft.event.persist.PersistentWorkflowI;
 import org.nrg.xft.event.persist.PersistentWorkflowUtils;
+import org.nrg.xft.exception.DBPoolException;
 import org.nrg.xft.schema.XFTManager;
 import org.nrg.xft.search.CriteriaCollection;
 import org.nrg.xft.security.UserI;
@@ -21,10 +24,10 @@ import org.nrg.xnat.utils.WorkflowUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+import java.sql.SQLException;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 import static org.nrg.xft.event.persist.PersistentWorkflowUtils.ADMIN_EXTERNAL_ID;
 
@@ -35,6 +38,16 @@ public class SharedScanCleanup extends AbstractInitializingTask {
     private final XnatUserProvider primaryAdminUserProvider;
     private static final String WORKFLOW_ACTION = "Unshare orphaned scans";
     private static final String SITE_TYPE = "site";
+    private static final String XNAT_IMAGESSCANDATA_ID = "xnat_imagescandata_id";
+    private static final String PROJECT = "project";
+    private static final String ORPHANED_SCANS = "SELECT scan.xnat_imagescandata_id, share.project "
+            + "FROM xnat_imageScandata scan "
+            + "LEFT JOIN xnat_imageScandata_share share "
+            + "ON scan.xnat_imagescandata_id = share.sharing_share_xnat_imagescandat_xnat_imagescandata_id "
+            + "LEFT JOIN (SELECT expt.id,share.project FROM xnat_experimentdata expt "
+            + "LEFT JOIN xnat_experimentdata_share share ON expt.id = share.sharing_share_xnat_experimentda_id) expts "
+            + "ON scan.image_session_id=expts.id AND share.project=expts.project "
+            + "WHERE share.project IS NOT NULL AND expts.project IS NULL;";
 
     @Autowired
     public SharedScanCleanup(final XnatAppInfo appInfo,
@@ -83,28 +96,26 @@ public class SharedScanCleanup extends AbstractInitializingTask {
         try {
             wrk = makeWorkflow(adminUser);
         } catch (PersistentWorkflowUtils.JustificationAbsent | PersistentWorkflowUtils.ActionNameAbsent |
-                PersistentWorkflowUtils.IDAbsent e) {
+                 PersistentWorkflowUtils.IDAbsent e) {
             log.error("Unable to create workflow for orphaned scan cleanup, which blocks said cleanup", e);
             return;
         }
 
         boolean success = true;
-        // Loop over projects to limit size of experiment list (avoid memory pressure)
-        for (final XnatProjectdata project : XnatProjectdata.getAllXnatProjectdatas(adminUser, true)) {
-            log.warn("Shared scan cleanup running on project {}", project.getId());
-            for (final Object experiment : project.getExperiments()) {
-                if (!(experiment instanceof XnatImagesessiondata)) {
-                    continue;
-                }
-                final XnatImagesessiondata session = (XnatImagesessiondata) experiment;
-                if (!session.getProject().equals(project.getId())) {
-                    // It's a shared session, which we'll handle when we get to its primary project
-                    continue;
-                }
-                if (!removeOrphanedScansFromSession(session, adminUser, wrk.buildEvent())) {
+        try {
+            final XFTTable scans = XFTTable.Execute(ORPHANED_SCANS, null, null);
+            while (scans.hasMoreRows()) {
+                final Map scanRow = scans.nextRowHash();
+                final Integer scanId = (Integer) scanRow.get(XNAT_IMAGESSCANDATA_ID);
+                final String projectId = (String) scanRow.get(PROJECT);
+                final XnatImagescandata scan = XnatImagescandata.getXnatImagescandatasByXnatImagescandataId(scanId, null, false);
+                if (!removeOrphanedScansFromSession(scan, projectId, adminUser, wrk.buildEvent())) {
                     success = false;
                 }
             }
+        } catch (SQLException | DBPoolException e) {
+            log.error("Unable to query for orphaned scans", e);
+            success = false;
         }
 
         try {
@@ -126,19 +137,10 @@ public class SharedScanCleanup extends AbstractInitializingTask {
                 ADMIN_EXTERNAL_ID, eventDetails);
     }
 
-    private boolean removeOrphanedScansFromSession(final XnatImagesessiondata session, final UserI user, final EventMetaI c) {
-        // Projects into which this session is shared
-        final Set<String> sessionSharedIntoProjects = session.getSharing_share().stream()
-                .map(XnatExperimentdataShareI::getProject)
-                .collect(Collectors.toSet());
-
+    private boolean removeOrphanedScansFromSession(final XnatImagescandata scan, final String projectId, final UserI user, final EventMetaI c) {
         boolean success = true;
-        for (final XnatImagescandataI scan : session.getScans_scan()) {
-            for (final XnatImagescandataShareI sharedScan : scan.getSharing_share()) {
-                if (sessionSharedIntoProjects.contains(sharedScan.getProject())) {
-                    continue;
-                }
-                // If a scan is shared into a project while its parent session is NOT, it's an orphaned shared scan
+        for (final XnatImagescandataShareI sharedScan : scan.getSharing_share()) {
+            if (StringUtils.equals(projectId, sharedScan.getProject())) {
                 try {
                     SaveItemHelper.authorizedRemoveChild(((XnatImagescandata) scan).getItem(),
                             XnatImagescandata.SCHEMA_ELEMENT_NAME + "/sharing/share",
