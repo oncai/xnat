9e15607decce5c78b024e380fe6685501324a9bb
Kate Alpert <katealpert@flywheel.io>
2022-10-07 12:09:41 +0000

    XNAT-7186 Use dedicated DiOI per receiver

--- src/main/java/org/nrg/dcm/id/ExtractorFromInstanceProvider.java
+++ src/main/java/org/nrg/dcm/id/ExtractorFromInstanceProvider.java
@@ -5,31 +5,8 @@ package org.nrg.dcm.id;
  * in the syntax of the custom-routing rule regex.
  *
  */
-public class ExtractorFromInstanceProvider extends ExtractorFromRuleProvider implements AeTitleAndPortAware {
-    private final RoutingExpressionFromInstanceProvider _routingExpressionFromInstanceProvider;
-    public ExtractorFromInstanceProvider( RoutingExpressionFromInstanceProvider routingExpressionFromInstanceProvider) {
+public class ExtractorFromInstanceProvider extends ExtractorFromRuleProvider {
+    public ExtractorFromInstanceProvider(RoutingExpressionFromInstanceProvider routingExpressionFromInstanceProvider) {
         super(routingExpressionFromInstanceProvider);
-        _routingExpressionFromInstanceProvider = routingExpressionFromInstanceProvider;
     }
-
-    @Override
-    public String getAeTitle() {
-        return _routingExpressionFromInstanceProvider.getAeTitle();
-    }
-
-    @Override
-    public void setAeTitle(String aeTitle) {
-        _routingExpressionFromInstanceProvider.setAeTitle( aeTitle);
-    }
-
-    @Override
-    public int getPort() {
-        return _routingExpressionFromInstanceProvider.getPort();
-    }
-
-    @Override
-    public void setPort(int port) {
-        _routingExpressionFromInstanceProvider.setPort( port);
-    }
-
 }
