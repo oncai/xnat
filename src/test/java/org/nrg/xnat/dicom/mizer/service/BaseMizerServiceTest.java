bfdabcd62b69522cdd3afcf13bcdbbda4a637c79
Kate Alpert <katealpert@flywheel.io>
2022-11-17 17:27:18 +0000

    XNAT-6742 fix unit tests

--- src/test/java/org/nrg/xnat/dicom/mizer/service/BaseMizerServiceTest.java
+++ src/test/java/org/nrg/xnat/dicom/mizer/service/BaseMizerServiceTest.java
@@ -15,7 +15,6 @@ import org.nrg.dicom.mizer.service.impl.MizerContextWithScript;
 import org.nrg.test.utils.TestFileUtils;
 import org.nrg.xnat.dicom.mizer.config.MizerServiceTestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
