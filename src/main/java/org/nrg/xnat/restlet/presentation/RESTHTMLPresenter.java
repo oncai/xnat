ac2b28cb9ac8e10f157f28679b9273ace6f2938a
Mohana Ramaratnam <mohanaramaratnam@flywheel.io>
2023-04-11 13:02:42 +0000

    Merged in XXX-218 (pull request #282)
    XXX-218
    
    * XXX-218
    
    
    Approved-by: Kate Alpert

--- src/main/java/org/nrg/xnat/restlet/presentation/RESTHTMLPresenter.java
+++ src/main/java/org/nrg/xnat/restlet/presentation/RESTHTMLPresenter.java
@@ -545,7 +545,7 @@ public class RESTHTMLPresenter extends PresentationA {
                             {
                                 vS= StringUtils.replace(vS, "<", "&#60;");
                             }
-							final String escapedString = dfr.isHtmlContent() ? vS : TurbineUtils.escapeParam(TurbineUtils.unescapeParam(vS))
+							final String escapedString = dfr.isHtmlContent() ? vS : TurbineUtils.escapeParam(TurbineUtils.unescapeParam(vS));
 							sb.append(escapedString);
 					    }
 					}
