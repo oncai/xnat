cb4164e33c8e9d5f53c1c25c5570a6f4f18538c5
Kevin Archie <kevinarchie@flywheel.io>
2023-04-25 13:35:18 +0000

    XNAT-7727 PrearcSessionResource::handleDelete should also clear session from study routing
    Approved-by: James Ransford
    Approved-by: wimibes

--- src/main/java/org/nrg/xnat/restlet/resources/prearchive/PrearcSessionResource.java
+++ src/main/java/org/nrg/xnat/restlet/resources/prearchive/PrearcSessionResource.java
@@ -21,6 +21,7 @@ import org.nrg.xdat.om.XnatProjectdata;
 import org.nrg.xft.XFTTable;
 import org.nrg.xft.exception.InvalidPermissionException;
 import org.nrg.xft.security.UserI;
+import org.nrg.xnat.actions.postArchive.ClearStudyRoutingAction;
 import org.nrg.xnat.archive.QueueBasedImageCommit;
 import org.nrg.xnat.helpers.prearchive.*;
 import org.nrg.xnat.helpers.prearchive.PrearcDatabase.SyncFailedException;
@@ -307,7 +308,11 @@ public final class PrearcSessionResource extends SecureResource {
                 getResponse().setStatus(CLIENT_ERROR_FORBIDDEN, "Unable to modify session data for destination project.");
                 return;
             }
+            final SessionData sd = PrearcDatabase.getSessionIfExists(session, timestamp, project);
             PrearcDatabase.deleteSession(session, timestamp, project);
+            if (sd != null) {
+                ClearStudyRoutingAction.doClear(sd.getTag());
+            }
         } catch (SessionException e) {
             log.warn("An error occurred trying to access the prearchive session {}/{}/{}: [{}] {}", project, timestamp, session, e.getError(), e.getMessage());
             getResponse().setStatus(getStatusForSessionException(e.getError()), e.getMessage());
