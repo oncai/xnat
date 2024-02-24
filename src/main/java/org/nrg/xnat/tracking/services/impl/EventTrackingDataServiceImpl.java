4a373eb2b6b93e9149305248eddaee4433974b76
Kate Alpert <katealpert@flywheel.io>
2024-02-24 00:00:28 +0000

    XNAT-7851 event tracking synchronization improvement (pull request #394)
    Approved-by: John Flavin
    Approved-by: James Ransford

--- src/main/java/org/nrg/xnat/tracking/services/impl/EventTrackingDataServiceImpl.java
+++ src/main/java/org/nrg/xnat/tracking/services/impl/EventTrackingDataServiceImpl.java
@@ -9,6 +9,7 @@
 
 package org.nrg.xnat.tracking.services.impl;
 
+import com.google.common.util.concurrent.Striped;
 import lombok.extern.slf4j.Slf4j;
 import org.nrg.framework.exceptions.NotFoundException;
 import org.nrg.xft.security.UserI;
@@ -20,11 +21,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
+import java.util.concurrent.locks.Lock;
 
 @Slf4j
 @Service
 public class EventTrackingDataServiceImpl implements EventTrackingDataService {
     private final EventTrackingDataHibernateService eventTrackingDataHibernateService;
+    private static final int NUM_LOCKS = 256;
+
+    @SuppressWarnings("UnstableApiUsage")  // Striped is marked as @Beta in our guava version, but not in current version
+    private static final Striped<Lock> LOCKS = Striped.lazyWeakLock(NUM_LOCKS);
 
     @Autowired
     public EventTrackingDataServiceImpl(final EventTrackingDataHibernateService eventTrackingDataHibernateService) {
@@ -59,8 +65,15 @@ public class EventTrackingDataServiceImpl implements EventTrackingDataService {
      * {@inheritDoc}
      */
     @Override
-    public synchronized void createOrUpdate(TrackableEvent eventData) throws IllegalAccessException {
-        eventTrackingDataHibernateService.createOrUpdate(eventData);
+    public void createOrUpdate(TrackableEvent eventData) throws IllegalAccessException {
+        @SuppressWarnings("UnstableApiUsage")
+        final Lock lock = LOCKS.get(eventData.getTrackingId());
+        lock.lock();
+        try {
+            eventTrackingDataHibernateService.createOrUpdate(eventData);
+        } finally {
+            lock.unlock();
+        }
     }
 
     /**
